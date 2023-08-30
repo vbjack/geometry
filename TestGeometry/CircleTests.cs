2ab81aac564e18a9cd9e9c96ea745153c47c2e06
andrey <aandivanov@gmail.com>
2023-08-30 18:18:40 +0300

    unit tests and move geometry

---
+++ TestGeometry/CircleTests.cs
@@ -0,0 +1,44 @@
+﻿using Geometry;
+
+namespace TestGeometry
+{
+    [Trait("Category", "Unit")]
+    public class CircleTests
+    {
+        [Fact]
+        public void CalculateArea_Success()
+        {
+            //arrange
+            double radius = 15.0;
+            Circle circle = new Circle(radius);
+
+            //act 
+            double area = circle.CalculateArea();
+
+            //asserts
+            Assert.Equal(Math.PI * Math.Pow(radius, 2), area);
+        }
+
+        [Fact]
+        public void CalculateArea_NegativeRadius()
+        {
+            //arrange
+            double radius = -5.3;
+            Circle circle = new Circle(radius);
+
+            //act and Assert
+            Assert.Throws<ArgumentException>(() => circle.CalculateArea());
+        }
+
+        [Fact]
+        public void CalculateArea_LargeRadius()
+        {
+            //arrange
+            double radius = 1E+154;
+            Circle circle = new Circle(radius);
+
+            //act and Assert
+            Assert.Throws<ArgumentException>(() => circle.CalculateArea());
+        }
+    }
+}
---
+++ TestGeometry/TriangleTests.cs
@@ -0,0 +1,67 @@
+﻿using Geometry;
+using Xunit;
+
+namespace TestGeometry
+{
+    [Trait("Category", "Unit")]
+    public class TriangleTests
+    {
+        [Fact]
+        public void CalculateArea_Generic_Success()
+        {
+            //arrange
+            double side1 = 3;
+            double side2 = 2.5;
+            double side3 = 5;
+            Triangle triangle = new Triangle(side1, side2, side3);
+
+            //act
+            double area = triangle.CalculateArea();
+
+            //assert
+            Assert.Equal(2.8497532787944992, area);
+        }
+
+        [Fact]
+        public void CalculateArea_RightTriangle_Success()
+        {
+            //arrange
+            double side1 = 3.0;
+            double side2 = 4.0;
+            double side3 = 5.0;
+            Triangle triangle = new Triangle(side1, side2, side3);
+
+            //act
+            double area = triangle.CalculateArea();
+
+            //assert
+            Assert.Equal(6.0, area);
+        }
+
+        [Fact]
+        public void CalculateArea_NegativeSides()
+        {
+            //arrange
+            double side1 = 3.0;
+            double side2 = -4.0;
+            double side3 = 5.0;
+            Triangle triangle = new Triangle(side1, side2, side3);
+
+            //act and Assert
+            Assert.Throws<ArgumentException>(() => triangle.CalculateArea());
+        }
+
+        [Fact]
+        public void CalculateArea_InvalidTriangle()
+        {
+            //arange
+            double side1 = 3.0;
+            double side2 = 4.0;
+            double side3 = 8.0;
+            Triangle triangle = new Triangle(side1, side2, side3);
+
+            //act and assert
+            Assert.Throws<ArgumentException>(() => triangle.CalculateArea());
+        }
+    }
+}
