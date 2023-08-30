26b51e42c2192a82ff26231db621750feb09daed
andrey <aandivanov@gmail.com>
2023-08-30 15:57:04 +0300

    circle calculation

--- Circle.cs
+++ Circle.cs
@@ -11,7 +11,16 @@
 
         public double CalculateArea()
         {
-            return 0;
+            if (Radius < 0)
+                throw new ArgumentException("Radius cannot be negative");
+
+            // Area will be more than max value
+            if (Radius > 7.564545572282618E+153)
+                throw new ArgumentException("Radius is too large to be represented as a double");
+
+            double area = Math.PI * Math.Pow(Radius, 2);
+
+            return area;
         }
     }
 }
